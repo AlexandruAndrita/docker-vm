1332b8a0c10ab367d0ffae9dac498cfb560303ff
AlexandruAndrita <andu.andritza@gmail.com>
2025-06-13 01:04:08 +0200

    adjusted app startup

--- app.py
+++ app.py
@@ -132,5 +132,4 @@ def create_plot(predictions, patient_information):
     return img_base64
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
